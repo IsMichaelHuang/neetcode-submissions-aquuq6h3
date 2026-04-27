f6ae525ff6115de66e21e56b5163ed4b9846cee8
Michael Huang <michaelhuangnot@gmail.com>
2026-04-27 10:56:17 -0700

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.py
@@ -0,0 +1,42 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        ROWS, COLS = len(grid), len(grid[0])
+        visit: set(tuple(int, int)) = set()
+        q = deque()
+
+        def layer(r, c):
+            # Do work
+            if (r, c) in visit or \
+                r >= ROWS or c >= COLS or \
+                r < 0 or c < 0 or \
+                grid[r][c] == -1:
+                return
+            
+            visit.add((r, c))
+            q.append((r, c))
+
+        # Find all teasure positions
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 0:
+                    visit.add((r, c))
+                    q.append((r, c))
+
+        dist = 0
+        while q:
+            for i in range(len(q)):
+                (r, c) = q.popleft()
+                grid[r][c] = dist
+
+                layer(r + 1, c)
+                layer(r - 1, c)
+                layer(r, c + 1)
+                layer(r, c - 1)
+
+            dist += 1
+                
+
+
+
+        
